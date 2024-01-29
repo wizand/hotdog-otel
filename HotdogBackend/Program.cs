65205b3667d4b900d16194ee5ff0ace98c15fb9c
Petteri Loisko <petteri.loisko@gmail.com>
2024-01-29 17:10:20 +0200

    Add rest of the demo components
    Add Frontend project
    Add common OTEL lib
    Add docker compose file for OTEL services

--- HotdogBackend/Program.cs
+++ HotdogBackend/Program.cs
@@ -1,3 +1,8 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+using OtelCommon;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,43 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+TraceActivities.Source = new System.Diagnostics.ActivitySource(ServiceNames.HotDogBackend);
+builder.Services.AddOpenTelemetry().ConfigureResource(
+    rb => rb
+    .AddService(ServiceNames.HotDogService) //This is the name of the service that will show up in jaeger
+        .AddAttributes(new List<KeyValuePair<string, object>> // These are attributes added to every trace
+            {
+                new KeyValuePair<string, object>("application", ServiceNames.HotDogBackend),
+                new KeyValuePair<string, object>("environment", "dev"),
+                new KeyValuePair<string, object>("version", "0.0.5")
+            }
+        )
+    ).WithTracing(builder => builder
+    .AddSource(ServiceNames.HotDogBackend)
+    .AddAspNetCoreInstrumentation()
+    .AddHttpClientInstrumentation()
+    .AddSqlClientInstrumentation().AddConsoleExporter()
+    .AddOtlpExporter(options =>
+    {
+        options.Endpoint = new Uri("http://localhost:6831"); //Jaeger endpoint
+        options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+    })
+    //.AddJaegerExporter(options =>
+    //{
+    //    options.AgentHost = "localhost"; // Or the appropriate host
+    //    options.AgentPort = 6831; // Default Jaeger agent UDP port
+    //}
+    //)
+    );
+    //.AddJaegerExporter(options =>
+    //{
+    //    options.AgentHost = "localhost"; // Or the appropriate host
+    //    options.AgentPort = 6831; // Default Jaeger agent UDP port
+    //})
+    //);
+
+
 
 var app = builder.Build();
 
---
+++ OtelCommon/TagNames.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace OtelCommon;
+
+    public static class TagNames
+    {
+        public const string UserLoggedIn = "user.logged_in";
+        public const string UserLockedOut = "user.logged_out";
+        public const string UserName = "user.name";
+        public const string CurrentOrderCount = "orders.current.count";
+        public const string OrderClientName = "order.client_name";
+
+        //Login and logout
+        public const string Login = "login";
+        public const string Logout = "logout";
+        public const string LoginSuccesful = "login.successful";
+
+        //Register user
+        public const string RegisterUser = "register";
+        public const string RegisterUserSuccessful = "register.successful";
+    }
+
+public static class ServiceNames
+    {
+        public const string HotDogService = "hotdog-service";
+        public const string HotDogFrontend = "hotdog-frontend";
+        public const string HotDogBackend = "hotdog-backend";
+    }
+
+    public static class TraceActivities
+    {
+    public static ActivitySource? Source = null;
+    }
+
